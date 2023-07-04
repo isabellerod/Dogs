073fc19a26c90d4abe61263104f3ce82672433dc
isabellerod <isabeller29@hotmail.com>
2023-07-04 15:39:16 -0300

    creation of Home and Header Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './Components/Header';
+import Home from './Components/Home';
 import Footer from './Components/Footer';
 
 const App = () => {
   return (
   <div>
-    <Header />
-    <Footer />
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   </div>
   );
 };
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import styles from './Header.module.css';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <div className={styles.header}>Header</div>
-  )
+    <div className={styles.header}>Header
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/login">Login / Sign Up</Link>
+      </nav>
+    </div>
+  );
 }
 
 export default Header
