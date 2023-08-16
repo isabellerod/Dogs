e3df9046ce0724aaf7894a44cfefd0c4aa3d8362
isabellerod <isabeller29@hotmail.com>
2023-08-16 20:24:32 -0300

    useContext to change header state when loged in

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,22 @@ import Header from './Components/Header';
 import Home from './Components/Home';
 import Footer from './Components/Footer';
 import Login from './Components/Login/Login';
+import { UserStorage } from './Hooks/UseContext';
 
 const App = () => {
   return (
-  <div>
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login/*" element={<Login />} />
-      </Routes>
-      <Footer />
-    </BrowserRouter>
-  </div>
+    <div>
+      <BrowserRouter>
+        <UserStorage>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login/*" element={<Login />} />
+          </Routes>
+          <Footer />
+        </UserStorage>
+      </BrowserRouter>
+    </div>
   );
 };
 
