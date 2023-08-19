21dc1b3ca8dbd5ab56e74b5b36f23db6efe534a1
isabellerod <isabeller29@hotmail.com>
2023-08-19 10:46:47 -0300

    token validation and logout config

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import LoginForm from './LoginForm';
 import LoginCreate from './LoginCreate';
 import LoginLostPassword from './LoginLostPassword';
 import LoginResetPassword from './LoginResetPassword';
+import { UserContext } from '../../Hooks/useContext';
 
 const Login = () => {
+  const { login } = React.useContext(UserContext);
+
+  if (login === true) return <Navigate to='/conta' />
+
   return (
     <div>
       <Routes>
--- src/Components/Login/LoginForm.jsx
+++ src/Components/Login/LoginForm.jsx
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom';
 import Input from '../Forms/Input';
 import Button from '../Forms/Button';
 import useForm from '../../Hooks/useForm';
-import { UserContext } from '../../Hooks/UseContext';
+import { UserContext } from '../../Hooks/useContext';
 
 const LoginForm = () => {
   const username = useForm();
   const password = useForm();
-  const { userLogin } = React.useContext(UserContext);
+  const { userLogin, error, loading } = React.useContext(UserContext);
 
   async function handleSubmit(event) {
     event.preventDefault();
@@ -24,7 +24,8 @@ const LoginForm = () => {
       <form action="" onSubmit={handleSubmit}>
         <Input label='User' type='text' name='username' {...username} />
         <Input label='Password' type='password' name='password' {...password} />
-        <Button>Log in</Button>
+        {loading ? <Button disabled>Loading...</Button> : <Button>Log in</Button>}
+        {error && <p>{error}</p>}
       </form>
       <Link to='/login/create'>Register</Link>
     </section>
