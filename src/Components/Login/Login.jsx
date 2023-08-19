6141a768612ceaec39d4e06f40f921efda6b093f
isabellerod <isabeller29@hotmail.com>
2023-08-19 18:16:02 -0300

    login background styled

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -5,6 +5,7 @@ import LoginCreate from './LoginCreate';
 import LoginLostPassword from './LoginLostPassword';
 import LoginResetPassword from './LoginResetPassword';
 import { UserContext } from '../../Hooks/useContext';
+import styles from './Login.module.css';
 
 const Login = () => {
   const { login } = React.useContext(UserContext);
@@ -12,14 +13,16 @@ const Login = () => {
   if (login === true) return <Navigate to='/conta' />
 
   return (
-    <div>
-      <Routes>
-        <Route path='/' element={<LoginForm />} />
-        <Route path='create' element={<LoginCreate />} />
-        <Route path='lost' element={<LoginLostPassword />} />
-        <Route path='reset' element={<LoginResetPassword />} />
-      </Routes>
-    </div>
+    <section className={styles.login}>
+      <div className={styles.forms}>
+        <Routes>
+          <Route path='/' element={<LoginForm />} />
+          <Route path='create' element={<LoginCreate />} />
+          <Route path='lost' element={<LoginLostPassword />} />
+          <Route path='reset' element={<LoginResetPassword />} />
+        </Routes>
+      </div>
+    </section>
   );
 };
 
